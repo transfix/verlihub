5e9d9c6ab1b020a72aa46bd9f745de7eab9fbfc1
Joe Rivera <j@jriv.us>
2025-12-15 00:39:52 -0600

    finally sorting out last crash from exploring threading and async edge cases

--- plugins/python/tests/test_stress_wrapper.cpp
+++ plugins/python/tests/test_stress_wrapper.cpp
@@ -226,7 +226,7 @@ protected:
         // Mock args: id, path, botname, opchatname, config_dir, starttime, config_name
         w_Targs* args = w_pack("lssssls", id, script_path.c_str(), "TestBot", "OpChat", ".", (long)0, "config");
         EXPECT_EQ(id, w_Load(args));
-        w_free_args(args);
+        free(args);  // Use free(), not w_free_args() - w_Load() copies strings with strdup()
         return id;
     }
 
@@ -241,7 +241,7 @@ protected:
                 free(res);
             }
         }
-        w_free_args(params);
+        free(params);  // Use free(), not w_free_args() - params are simple
     }
 };
 
@@ -268,7 +268,7 @@ TEST_F(PythonWrapperTest, CallHook) {
     // Call OnTimer (expects double, but script returns 1)
     w_Targs* params = w_pack("d", 123.456);
     w_Targs* res = w_CallHook(id, W_OnTimer, params);
-    w_free_args(params);
+    free(params);  // Use free(), not w_free_args()
 
     ASSERT_NE(res, nullptr);
     long ret_val;
@@ -279,7 +279,7 @@ TEST_F(PythonWrapperTest, CallHook) {
     // Call OnParsedMsgChat (ss: nick, data)
     params = w_pack("ss", "test_user", "hello");
     res = w_CallHook(id, W_OnParsedMsgChat, params);
-    w_free_args(params);
+    free(params);  // Use free(), not w_free_args()
 
     ASSERT_NE(res, nullptr);
     w_unpack(res, "l", &ret_val);
